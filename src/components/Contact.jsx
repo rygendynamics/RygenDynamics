89d7c61e4770e63775e29a5cb80adefc6c5fa3c5
Rygen Dynamics <rygendynamics@gmail.com>
2026-03-03 17:59:03 +0530

    Increase cropping for Get in Touch section template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
             </p>
 
             <div className="contact-3d-viewer">
-              <SplineViewer sceneUrl="https://prod.spline.design/V-X5Ty2sZktcuHAP/scene.splinecode" clipHeight={80} />
+              <SplineViewer sceneUrl="https://prod.spline.design/V-X5Ty2sZktcuHAP/scene.splinecode" clipHeight={100} />
             </div>
 
             <div className="contact-methods">
